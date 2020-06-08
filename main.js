2c6b26ffcf915f69a28dce505a8de6ebe7e36135
Giorgi Shengelaia <g.shengelaia2001@gmail.com>
2020-06-08 17:04:03 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,162 @@
+'use strict';
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${title} by ${author}, ${pages} pages, ${read ? 'read it' : 'not read yet'}`;
+  };
+}
+
+function addBookToLibrary(obj) {
+  myLibrary.push(obj);
+}
+
+function showModal() {
+  const modal = document.querySelector('.modal');
+  modal.classList.toggle('show');
+
+  const textInputs = document.querySelectorAll('[type="text"]');
+  textInputs.forEach(textInput => {
+    textInput.value = '';
+  });
+}
+
+function addBtnCols(task) {
+  const colBtn = document.createElement('div');
+  colBtn.classList.add('col-btn');
+
+  const emptyDiv = document.createElement('div');
+  emptyDiv.classList.add('empty');
+  colBtn.appendChild(emptyDiv);
+
+  const btnDiv = document.createElement('div');
+  btnDiv.classList.add('btn-div');
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    const btn = document.createElement('button');
+    btn.dataset.targetTitle = myLibrary[i].title;
+
+    if (task === 'delete') {
+      btn.classList.add('del-btn')
+      btn.textContent = 'delete';
+      btn.addEventListener('click', deleteBook);
+    } else if (task === 'toggleRead') {
+      btn.textContent = 'toggle Read';
+      btn.addEventListener('click', toggleRead)
+    }
+
+    btnDiv.appendChild(btn);
+  }
+
+  colBtn.appendChild(btnDiv);
+
+  const infoDiv = document.querySelector('.info-div');
+  infoDiv.appendChild(colBtn);
+}
+
+function addDescCols() {
+  for (let property in myLibrary[0]) {
+    if (property === 'info') continue;
+
+    const column = document.createElement('div');
+    column.classList.add('column');
+
+    const colTitle = document.createElement('p');
+    colTitle.classList.add('col-title')
+    colTitle.textContent = property;
+    column.appendChild(colTitle);
+
+    for (let i = 0; i < myLibrary.length; i++) {
+      const value = document.createElement('p');
+      value.textContent = myLibrary[i][property];
+      column.appendChild(value);
+    }
+
+    const infoDiv = document.querySelector('.info-div');
+    infoDiv.appendChild(column);
+  }
+}
+
+function render() {
+  const mainDiv = document.querySelector('.main');
+
+  const infoDiv = document.createElement('div');
+  infoDiv.classList.add('info-div');
+  mainDiv.appendChild(infoDiv);
+
+  addDescCols();
+
+  addBtnCols('delete');
+  addBtnCols('toggleRead')
+}
+
+function clearInfo() {
+  document.querySelector('.info-div').remove();
+}
+
+function getTargetBookIndex(e) {
+  const bookTitle = e.target.dataset.targetTitle;
+  const bookIndex = myLibrary.findIndex(book => book.title === bookTitle);
+  return bookIndex;
+}
+
+function deleteBook(e) {
+  myLibrary.splice(getTargetBookIndex(e), 1);
+
+  clearInfo();
+  render();
+}
+
+function toggleRead(e) {
+  const targetBook = myLibrary[getTargetBookIndex(e)];
+
+  targetBook.read = targetBook.read ? false : true;
+
+  clearInfo();
+  render();
+}
+
+function readAndWrite() {
+  const book = new Book();
+  console.log(book);
+
+  const textInputs = document.querySelectorAll('[type="text"]');
+  textInputs.forEach(textInput => {
+    book[textInput.name] = textInput.value;
+    textInput.value = '';
+  });
+
+  const radioInputs = document.querySelectorAll('[type="radio"]')
+  radioInputs.forEach(radioInput => {
+    if (radioInput.checked) book[radioInput.name] = radioInput.value;
+  });
+
+  console.log(book);
+
+  addBookToLibrary(book);
+  showModal();
+  clearInfo();
+  render();
+}
+
+function addEventListeners() {
+  const addBtn = document.querySelector('.add-btn');
+  addBtn.addEventListener('click', showModal);
+
+  const okBtn = document.querySelector('.ok-btn');
+  okBtn.addEventListener('click', readAndWrite);
+
+  const cancelBtn = document.querySelector('.cancel-btn');
+  cancelBtn.addEventListener('click', showModal);
+}
+
+const myLibrary = [];
+
+addBookToLibrary(new Book('Hobbit', 'J. R. R. Tolkien', 288, true));
+addBookToLibrary(new Book('The Temple of the golden pavillion', 'Yukio Mishima', 270, false));
+
+render();
+addEventListeners();
