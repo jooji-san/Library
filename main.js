3e07a2e8ba758abe9ca1b05e9c1315042a694c8f
Giorgi Shengelaia <g.shengelaia2001@gmail.com>
2020-07-01 20:03:25 +0400

    added form element

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 class Book {
   constructor(title, author, pages, read) {
@@ -7,15 +7,17 @@ class Book {
     this.pages = pages;
     this.read = read;
     this.info = function () {
-      return `${title} by ${author}, ${pages} pages, ${read ? 'read it' : 'not read yet'}`;
+      return `${title} by ${author}, ${pages} pages, ${
+        read ? "read it" : "not read yet"
+      }`;
     };
   }
 }
 
 const library = (function () {
   let myLibrary = [
-    new Book('Hobbit', 'J. R. R. Tolkien', 288, true),
-    new Book('The Temple of the golden pavillion', 'Yukio Mishima', 270, false),
+    new Book("Hobbit", "J. R. R. Tolkien", 288, true),
+    new Book("The Temple of the golden pavillion", "Yukio Mishima", 270, false),
   ];
 
   function get() {
@@ -37,7 +39,7 @@ const library = (function () {
 
   function getTargetBookIndex(e) {
     const bookTitle = e.target.dataset.targetTitle;
-    const bookIndex = myLibrary.findIndex(book => book.title === bookTitle);
+    const bookIndex = myLibrary.findIndex((book) => book.title === bookTitle);
     return bookIndex;
   }
 
@@ -45,30 +47,30 @@ const library = (function () {
     const book = new Book();
 
     const textInputs = document.querySelectorAll('[type="text"]');
-    textInputs.forEach(textInput => {
-      book[textInput.name] = textInput.value || 'n/a';
-      textInput.value = '';
+    textInputs.forEach((textInput) => {
+      book[textInput.name] = textInput.value || "n/a";
+      textInput.value = "";
     });
 
     const radioInputs = document.querySelectorAll('[type="radio"]');
-    radioInputs.forEach(radioInput => {
+    radioInputs.forEach((radioInput) => {
       if (radioInput.checked) {
         book.read = radioInput.value;
       }
     });
-    if (!book['read']) {
-      book.read = 'n/a';
+    if (!book["read"]) {
+      book.read = "n/a";
     }
 
     myLibrary.push(book);
   }
 
-  return { get, set, remove, add, toggleRead }
+  return { get, set, remove, add, toggleRead };
 })();
 
 const save = (function () {
   function init() {
-    if (localStorage.getItem('myLibrary')) {
+    if (localStorage.getItem("myLibrary")) {
       get();
     } else {
       set();
@@ -76,47 +78,46 @@ const save = (function () {
   }
 
   function get() {
-    library.set(JSON.parse(localStorage.getItem('myLibrary')));
+    library.set(JSON.parse(localStorage.getItem("myLibrary")));
   }
 
   function set() {
-    localStorage.setItem('myLibrary', JSON.stringify(library.get()));
+    localStorage.setItem("myLibrary", JSON.stringify(library.get()));
   }
 
-  return { init, set }
+  return { init, set };
 })();
 
 const displayController = (function () {
   function toggleModal() {
-    const modal = document.querySelector('.modal');
-    modal.classList.toggle('show');
+    const modal = document.querySelector(".modal");
+    modal.classList.toggle("show");
 
     const textInputs = document.querySelectorAll('[type="text"]');
-    textInputs.forEach(textInput => {
-      textInput.value = '';
+    textInputs.forEach((textInput) => {
+      textInput.value = "";
     });
   }
 
   function render() {
+    const mainDiv = document.querySelector(".main");
 
-    const mainDiv = document.querySelector('.main');
-
-    const infoDiv = document.createElement('div');
-    infoDiv.classList.add('info-div');
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("info-div");
     mainDiv.appendChild(infoDiv);
 
     addBookCards();
     addDescs();
-    addBtn('delete');
-    addBtn('toggleRead');
-  };
+    addBtn("delete");
+    addBtn("toggleRead");
+  }
 
   function addBookCards() {
-    const infoDiv = document.querySelector('.info-div');
+    const infoDiv = document.querySelector(".info-div");
 
     for (let i = 0; i < library.get().length; i++) {
-      const card = document.createElement('div');
-      card.classList.add('card');
+      const card = document.createElement("div");
+      card.classList.add("card");
       infoDiv.appendChild(card);
     }
   }
@@ -126,9 +127,9 @@ const displayController = (function () {
       const card = document.querySelector(`.card:nth-of-type(${i + 1})`);
 
       for (let property in library.get()[i]) {
-        if (property === 'info') continue;
+        if (property === "info") continue;
 
-        const bookProperty = document.createElement('div');
+        const bookProperty = document.createElement("div");
         bookProperty.classList.add(`${property}-div`);
         bookProperty.textContent = library.get()[i][property];
         card.appendChild(bookProperty);
@@ -138,16 +139,16 @@ const displayController = (function () {
 
   function addBtn(task) {
     for (let i = 0; i < library.get().length; i++) {
-      const btn = document.createElement('button');
+      const btn = document.createElement("button");
       btn.dataset.targetTitle = library.get()[i].title;
 
-      if (task === 'delete') {
-        btn.classList.add('del-btn')
-        btn.textContent = 'delete';
-        btn.addEventListener('click', handleDeleteBtn);
-      } else if (task === 'toggleRead') {
-        btn.textContent = 'toggle Read';
-        btn.addEventListener('click', handleToggleReadBtn);
+      if (task === "delete") {
+        btn.classList.add("del-btn");
+        btn.textContent = "delete";
+        btn.addEventListener("click", handleDeleteBtn);
+      } else if (task === "toggleRead") {
+        btn.textContent = "toggle Read";
+        btn.addEventListener("click", handleToggleReadBtn);
       }
 
       const card = document.querySelector(`.card:nth-of-type(${i + 1})`);
@@ -166,7 +167,7 @@ const displayController = (function () {
   }
 
   function clear() {
-    document.querySelector('.info-div').remove();
+    document.querySelector(".info-div").remove();
   }
 
   function refresh() {
@@ -176,27 +177,22 @@ const displayController = (function () {
   }
 
   (function addEventListeners() {
-    const addBtn = document.querySelector('.add-btn');
-    addBtn.addEventListener('click', toggleModal);
+    const addBtn = document.querySelector(".add-btn");
+    addBtn.addEventListener("click", toggleModal);
 
-    const okBtn = document.querySelector('.ok-btn');
-    okBtn.addEventListener('click', handleOkButton);
-
-    const cancelBtn = document.querySelector('.cancel-btn');
-    cancelBtn.addEventListener('click', toggleModal);
+    const form = document.querySelector("form");
+    form.addEventListener("submit", handleFormSubmit);
+    form.addEventListener("reset", toggleModal);
   })();
 
-  function handleOkButton() {
+  function handleFormSubmit() {
     library.add();
     toggleModal();
     refresh();
   }
 
-  return { render }
+  return { render };
 })();
-
-// addBookToLibrary(new Book('Hobbit', 'J. R. R. Tolkien', 288, true));
-// addBookToLibrary(new Book('The Temple of the golden pavillion', 'Yukio Mishima', 270, false));
 
 save.init();
 displayController.render();
